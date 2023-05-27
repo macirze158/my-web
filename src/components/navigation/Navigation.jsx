5aeb9b8a16d0908bc5837426ab2fbe328434c1ae
macirze158 <macirze158@student.polsl.pl>
2023-05-27 19:23:36 +0200

    navbar added

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './navigation.css'
+import {FaHome} from 'react-icons/fa'
+import {SiAboutdotme} from 'react-icons/si'
+import {GiOpenBook} from 'react-icons/gi'
+import {AiFillProject} from 'react-icons/ai'
+import {AiFillMessage} from 'react-icons/ai'
+import { useState } from 'react'
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
+    <nav> 
+      <a href="#"><FaHome /></a>
+      <a href="#about"><SiAboutdotme /></a>
+      <a href="#skills"><GiOpenBook /></a>
+      <a href="#projects"><AiFillProject /></a>
+      <a href="#contact"><AiFillMessage /></a>
+    </nav>
   )
 }
 
-export default Navigation
+export default Navigation 
