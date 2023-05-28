905ed03357d0b177ccfe28dcd25ef99fe2b64326
macirze158 <macirze158@student.polsl.pl>
2023-05-29 00:41:12 +0200

    contact and footer added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineMailOutline} from 'react-icons/md'
+import {BsInstagram} from 'react-icons/bs'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Message me</h5>
+      <h2>Contact</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+
+          <a href="mailto:maciej.rzeznikk@gmail.com" target="_blank" rel="noreferrer">
+            <article className="contact__option">
+              <MdOutlineMailOutline className="contact__option-icon"/>
+              <h4>Email</h4>
+              <h5>maciej.rzeznikk@gmail.com</h5>
+            </article>
+          </a>
+
+          <a href="https://ig.me/m/maciekmrz" target="_blank" rel="noreferrer">
+            <article className="contact__option">
+              <BsInstagram className="contact__option-icon"/>
+              <h4>Instagram</h4>
+              <h5>maciekmrz</h5>
+            </article>
+          </a>
+
+          <a href="https://api.whatsapp.com/send?phone=+48795702234" target="_blank" rel="noreferrer">
+            <article className="contact__option">
+              <BsWhatsapp className="contact__option-icon"/>
+              <h4>WhatsApp</h4>
+              <h5>Maciej Rzeźnik</h5>
+            </article>
+          </a>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,17 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__name">MACIEJ RZEŹNIK</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </footer>
   )
 }
 
